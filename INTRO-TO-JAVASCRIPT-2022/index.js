1346dece5559945c49ac2c34e81f31052342c930
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2022-11-16 13:14:56 -0500

    asdf

--- INTRO-TO-JAVASCRIPT-2022/index.js
+++ INTRO-TO-JAVASCRIPT-2022/index.js
@@ -1,6 +1,25 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
-  point: document.querySelectorAll(".point"),
+  box: document.getElementById("big-black-box"),
+  points: document.querySelector(".point"),
 };
-console.log(DOMSelectors.point);
+
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.textContent = "This is now a bigger red box ";
+  text.style.fontSize = "40px";
+}
+
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
+
+function changeLi() {
+  DOMSelectors.points.forEach((point) => {
+    point.addEventListener("click", function () {
+      point.innerHTML("Hello");
+    });
+  });
+}
+changeLi();
