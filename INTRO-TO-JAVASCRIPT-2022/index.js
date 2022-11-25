d77b6469b6b23f08c50043ccba380feaa6bd4418
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2022-11-25 17:07:00 -0500

    asdf

--- INTRO-TO-JAVASCRIPT-2022/index.js
+++ INTRO-TO-JAVASCRIPT-2022/index.js
@@ -1,25 +1,7 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("big-black-box"),
-  points: document.querySelector(".point"),
+  box: document.getElementById("container-box"),
+  input: document.querySelector("#input"),
 };
-
-function backgroundAndText(background, text) {
-  background.style.backgroundColor = "blue";
-  text.textContent = "This is now a bigger blue box ";
-  text.style.fontSize = "40px";
-}
-
-DOMSelectors.button.addEventListener("click", function () {
-  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-});
-
-function changeLi() {
-  DOMSelectors.points.forEach((point) => {
-    point.addEventListener("click", function () {
-      point.innerHTML("Hello");
-    });
-  });
-}
-changeLi();
+const cat = "meow";
+DOMSelectors.box.insertAdjacentHTML("beforebegin", "<h1>We are an </h1>");
