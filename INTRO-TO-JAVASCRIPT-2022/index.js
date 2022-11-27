d8d00a098431f6430d883d46a8e6996593c70d05
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2022-11-27 17:07:40 -0500

    fin

--- INTRO-TO-JAVASCRIPT-2022/index.js
+++ INTRO-TO-JAVASCRIPT-2022/index.js
@@ -1,13 +1,44 @@
-const DOMSelectors = {
-  button: document.getElementById("btn"),
-  box: document.getElementById("container-box"),
-  input: document.querySelector("#input"),
-};
-const cat = "meow";
-DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are ${cat}</h1>`);
+// var
 
-DOMSelectors.button.addEventListener("click", function () {
-  let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p> ${input}</p>`);
-  DOMSelectors.input.value = "";
+const addTask = document.getElementById(`add-task`);
+const taskContainer = document.getElementById(`task-container`);
+const inputTask = document.getElementById(`input-task`);
+
+// event listener
+
+addTask.addEventListener(`click`, function () {
+  let task = document.createElement(`div`);
+  task.classList.add(`task`);
+  console.log(task);
+
+  let li = document.createElement(`li`);
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+
+  let checkButton = document.createElement("button");
+  checkButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
+  checkButton.classList.add(`checkTask`);
+  task.appendChild(checkButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  deleteButton.classList.add(`deleteTask`);
+  task.appendChild(deleteButton);
+
+  if (inputTask.value === "") {
+    alert(`Enter Task`);
+  } else {
+    taskContainer.appendChild(task);
+  }
+
+  inputTask.value = "";
+
+  checkButton.addEventListener(`click`, function () {
+    checkButton.parentElement.style.textDecoration = "line-through";
+  });
+
+  deleteButton.addEventListener(`click`, function (e) {
+    let target = e.target;
+    target.parentElement.parentElement.remove();
+  });
 });
